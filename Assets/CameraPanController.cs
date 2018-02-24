9eae88f11547d72900618668436f210c86a5480a
Omar Iglesk <marloo-_-@hotmail.com>
2018-02-24 14:29:26 +0200

    Add Camera as child of player add pan limit to camera in player

---
+++ Assets/CameraPanController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraPanController : MonoBehaviour
+{
+   public Vector2 panLimit;
+
+   void Update()
+   {
+      Vector3 pos = transform.position;
+
+      pos.x = Mathf.Clamp(pos.x, -panLimit.x, panLimit.x);
+
+      pos.y = Mathf.Clamp(pos.y, -panLimit.y, panLimit.y);
+
+      transform.position = pos;
+   }
+}
